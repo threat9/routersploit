eb2b21ab0b9b5eb9a349f8c48b7ab9b5c0be5609
fwkz <f4wkes@gmail.com>
2016-04-14 19:28:05 +0200

    Adding new utils function to routersploit.__init__.py

--- routersploit/__init__.py
+++ routersploit/__init__.py
@@ -1,9 +1,9 @@
-from routersploit.utils import print_error, print_status, print_success, print_table, sanitize_url, LockedIterator
+from routersploit.utils import (
+    print_error, print_status, print_success,
+    print_table, print_info, sanitize_url,
+    LockedIterator,
+)
+
 from routersploit import exploits
 from routersploit import wordlists
-
-all = [
-    print_error, print_status, print_success,
-    exploits,
-]
 
