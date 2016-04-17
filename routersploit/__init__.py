2dc1d64849c5bf50d4276adf1af0bf610901aae0
fwkz <f4wkes@gmail.com>
2016-04-17 14:41:40 +0200

    Fixing unmerged file.

--- routersploit/__init__.py
+++ routersploit/__init__.py
@@ -1,9 +1,4 @@
 from routersploit.utils import (
-<<<<<<< d4fb4d39165c40b179c139a9d568e5ba87553cc8
-    print_error, print_status, print_success,
-    print_table, print_info, sanitize_url,
-    LockedIterator, random_text,
-=======
     print_error,
     print_status,
     print_success,
@@ -11,8 +6,8 @@ from routersploit.utils import (
     print_info,
     sanitize_url,
     LockedIterator,
+    random_text,
     http_request,
->>>>>>> Adding http_request() idea.
 )
 
 from routersploit import exploits
