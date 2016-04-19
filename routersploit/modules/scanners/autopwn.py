78c76930840c57d2760d3b24a605d7fdff125a59
fwkz <f4wkes@gmail.com>
2016-04-19 22:16:39 +0200

    Module cosmetics and PEP8 fixes.

--- routersploit/modules/scanners/autopwn.py
+++ routersploit/modules/scanners/autopwn.py
@@ -1,7 +1,13 @@
-from routersploit import *
 from os import listdir
-from os.path import isfile, join
 import imp
+
+from routersploit import (
+    exploits,
+    print_error,
+    print_success,
+    print_status,
+    print_info,
+)
 
 
 class Exploit(exploits.Exploit):
@@ -12,54 +18,50 @@ class Exploit(exploits.Exploit):
         'name': 'AutoPwn',
         'description': 'Scanner module for all vulnerabilities.',
         'author': [
-            'Marcin Bury <marcin.bury[at]reverse-shell.com>', # routersploit module
+            'Marcin Bury <marcin.bury[at]reverse-shell.com>',  # routersploit module
          ],
     }
 
-    target = exploits.Option('', 'Target address e.g. http://192.168.1.1') # target address
-    port = exploits.Option(80, 'Target port') # default port
+    target = exploits.Option('', 'Target address e.g. http://192.168.1.1')  # target address
+    port = exploits.Option(80, 'Target port')  # default port
 
     def run(self):
-        exploits = []
         rootpath = 'routersploit/modules/'
         path = 'exploits'
 
         modules = []
-        for device in listdir(rootpath+path):
+        for device in listdir(rootpath+path):  # TODO refactor this, using load_modules() from core
             if not device.endswith(".py") and not device.endswith(".pyc"):
                 for f in listdir(rootpath+path + "/" + device):
                     if f.endswith(".py") and f != "__init__.py":
                         modules.append(device + "/" + f[:-3])
 
-        vulns = []
+        vulnerabilities = []
         for module_name in modules:
-            f = path + "/"+ module_name
-            
+            f = "".join((path, "/", module_name))
+
             module = imp.load_source('module', rootpath + f + '.py')
             exploit = module.Exploit()
 
             exploit.target = self.target
             exploit.port = self.port
 
-            res = exploit.check()
+            response = exploit.check()
 
-            if res is True:
+            if response is True:
                 print_success("{} is vulnerable".format(f))
-                vulns.append(f)
-            elif res is False:
+                vulnerabilities.append(f)
+            elif response is False:
                 print_error("{} is not vulnerable".format(f))
             else:
                 print_status("{} could not be verified".format(f))
 
-        print 
-        if len(vulns):
-            print_success("Device is vulnerable!")
-            for v in vulns:
-                print " - {}".format(v)
+        if vulnerabilities:
+            print_success("\nDevice is vulnerable!")
+            for v in vulnerabilities:
+                print_info(" - {}".format(v))
         else:
-            print_error("Device is not vulnerable to any exploits!")
-        print
+            print_error("Device is not vulnerable to any exploits!\n")
         
     def check(self):
         print_error("Check method is not available")
-
