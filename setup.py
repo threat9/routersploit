562bdba07e0f4d635f0cb08272358f8ab1d1995c
Bouncingbunny <mtsneffels13@gmail.com>
2016-04-26 19:31:12 -0400

    Added a setup.py file that sets up path and stuff when run.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os, sys
+
+if os.geteuid() != 0:
+	print("%s needs root privildeges\nTry running 'sudo %s'" % (sys.argv[0], sys.argv[0]))
+	sys.exit(1)
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+HOME = os.path.expanduser('~')
+
+''' Create a file to start rsf.py '''
+with open(PATH+'/rsfconsole', 'w+') as f:
+	f.write('#!/bin/bash\n%s/rsf.py' % PATH)
+	f.close()
+	os.system('/bin/chmod +x %s/rsfconsole' % PATH)
+
+''' Write to .bashrc '''
+with open(HOME + '/.bashrc', 'r+') as f:
+	a = 'export PATH=$PATH:%s' % PATH
+	if a not in f.read():
+		f.write('\n%s\n' % a)
+	f.close()
+
+os.system('export PATH=$PATH:$(pwd)')
+
+os.system('/usr/bin/pip install -r %s/requirements.txt' % PATH)
