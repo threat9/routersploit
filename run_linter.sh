4bacabd35063fe24bf24f6afe25116d03c383837
fwkz <f4wkes@gmail.com>
2016-07-17 14:12:40 +0200

    Adding linter to Travis build. Expecting to fail.

---
+++ run_linter.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+IFS=' '
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+MODULES_PATH=./routersploit/modules
+FAILURE=0
+
+PEP=$(pep8 --ignore E501 $MODULES_PATH)
+PYFLAKES=$(pyflakes $MODULES_PATH)
+
+if [ "$PEP" ]; then
+    echo -e "${RED}- PEP8 violations:${NC}"
+    echo -e $PEP
+    echo ""
+    FAILURE=1
+else
+    echo -e "${GREEN}+ PEP8${NC}"
+fi
+
+if [ "$PYFLAKES" ]; then
+    echo -e "\n${RED}- pyflakes violations:${NC}"
+    echo -e $PYFLAKES
+    echo "\n"
+    FAILURE=1 
+else
+    echo -e "${GREEN}+ pyflakes${NC}"
+fi
+
+exit $FAILURE
