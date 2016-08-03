8631b17ba547513692dc45c0189afa4e05bc59a7
fwkz <f4wkes@gmail.com>
2016-08-03 18:21:23 +0200

    Summary of threaded autopwn

--- routersploit/modules/scanners/autopwn.py
+++ routersploit/modules/scanners/autopwn.py
@@ -4,8 +4,9 @@ from routersploit import (
     print_error,
     print_success,
     print_status,
+    print_info,
     utils,
-    LockedIterator
+    LockedIterator,
 )
 
 
@@ -33,7 +34,16 @@ class Exploit(exploits.Exploit):
 
     def run(self):
         data = LockedIterator(utils.iter_modules(utils.EXPLOITS_DIR))
+        self.vulnerabilities = []
         self.run_threads(self.threads, self.target_function, data)
+
+        if self.vulnerabilities:
+            print_info()
+            print_success("Device is vulnerable!")
+            for v in self.vulnerabilities:
+                print_info(" - {}".format(v))
+        else:
+            print_error("Device is not vulnerable to any exploits!\n")
 
     def check(self):
         raise NotImplementedError("Check method is not available")
@@ -56,6 +66,7 @@ class Exploit(exploits.Exploit):
 
                 if response is True:
                     print_success("{} {} is vulnerable".format(name, exploit))
+                    self.vulnerabilities.append(exploit)
                 elif response is False:
                     print_error("{} {} is not vulnerable".format(name, exploit))
                 else:
