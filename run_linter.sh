ed0973b0d76d618e3771e0de601f457991da4d50
fwkz <f4wkes@gmail.com>
2016-09-05 20:16:59 +0200

    More flexible run_linter.sh script

--- run_linter.sh
+++ run_linter.sh
@@ -4,11 +4,16 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-MODULES_PATH=./routersploit/modules
+
+MODULES_PATH=./routersploit
 FAILURE=0
 
+if [ "$1" ]; then
+    MODULES_PATH="$MODULES_PATH/$1"
+fi
+
 FLAKE8_IGNORED_RULES='E501,W503'
-FLAKE8=$(flake8 --ignore=$FLAKE8_IGNORED_RULES $MODULES_PATH)
+FLAKE8=$(flake8 --exclude=__init__.py --ignore=$FLAKE8_IGNORED_RULES $MODULES_PATH)
 
 if [ "$FLAKE8" ]; then
     echo -e "\n${RED}- flake8 violations:${NC}"
