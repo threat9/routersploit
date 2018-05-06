822deebaa85c8518ff80248c5240b3cab4170076
Marcin Bury <marcin.bury@reverse-shell.com>
2018-05-06 17:59:14 +0200

    Removing template functionality (#389)

--- rsf.py
+++ rsf.py
@@ -2,23 +2,16 @@
 
 from __future__ import print_function
 
-import argparse
 import logging.handlers
 
 from routersploit.interpreter import RoutersploitInterpreter
 
-log_handler = logging.handlers.RotatingFileHandler(filename='routersploit.log', maxBytes=500000)
-log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s       %(message)s')
+log_handler = logging.handlers.RotatingFileHandler(filename="routersploit.log", maxBytes=500000)
+log_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s       %(message)s")
 log_handler.setFormatter(log_formatter)
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
 LOGGER.addHandler(log_handler)
-
-parser = argparse.ArgumentParser(description='RouterSploit - Router Exploitation Framework')
-parser.add_argument('-a',
-                    '--add-exploit',
-                    metavar='exploit_path',
-                    help='Add exploit using default template.')
 
 
 def routersploit():
