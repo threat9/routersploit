6f1d6134472f2559a56126aeaa3630a39e407e3c
lucyoa <marcin@threat9.com>
2018-05-08 10:15:14 +0200

    Adding travis

---
+++ .travis/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    source ~/.venv/bin/activate
+fi
+
+python -m pytest -n16 tests/
