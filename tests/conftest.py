ff23fe85362d3cc7b25acfc2e194b67a864c78f7
lucyoa <marcin@threat9.com>
2018-05-11 22:20:39 +0200

    Adding tests for exploits info

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,46 @@ def target():
     with HttpServiceMock("127.0.0.1", 0) as target_:
         yield target_
 
+@pytest.fixture(scope="session")
+def empty_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.EMPTY_RESPONSE) as http_service:
+        yield http_service
+
+
+@pytest.fixture(scope="session")
+def trash_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.TRASH) as http_service:
+        yield http_service
+
+
+@pytest.fixture(scope="session")
+def not_found_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.NOT_FOUND) as http_service:
+        yield http_service
+
+
+@pytest.fixture(scope="session")
+def redirect_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.REDIRECT) as http_service:
+        yield http_service
+
+
+@pytest.fixture(scope="session")
+def error_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.ERROR) as http_service:
+        yield http_service
+
+
+@pytest.fixture(scope="session")
+def timeout_target():
+    with HttpScenarioService("127.0.0.1", 0,
+                             HttpScenario.TIMEOUT) as http_service:
+        yield http_service
 
 @pytest.fixture
 def generic_target():
