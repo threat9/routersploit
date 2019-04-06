788ec9b256b217c74782f0f07e32eb00d95ddd0e
Leonardo Taccari <iamleot@gmail.com>
2019-04-06 12:22:10 +0200

    Avoid hardcoded relative paths and use MODULES_DIR instead
    Commit id 9380c04707da979f3081e6b3678c00f7f612bd95 (probably
    accidentally) removed EXPLOITS_DIR use leading to loading
    routersploit/modules/exploits relative to the current directory
    instead of the installed ones in MODULES_DIR.

--- routersploit/modules/scanners/autopwn.py
+++ routersploit/modules/scanners/autopwn.py
@@ -34,8 +34,8 @@ class Exploit(Exploit):
         self.vulnerabilities = []
         self.creds = []
         self.not_verified = []
-        self._exploits_directories = [path.join("routersploit/modules/exploits/", module) for module in self.modules]
-        self._creds_directories = [path.join("routersploit/modules/creds/", module) for module in self.modules]
+        self._exploits_directories = [path.join(utils.MODULES_DIR, "exploits", module) for module in self.modules]
+        self._creds_directories = [path.join(utils.MODULES_DIR, "creds", module) for module in self.modules]
 
     def run(self):
         self.vulnerabilities = []
