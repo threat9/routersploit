9c7ab09184aba8f1bb1c35fa9c66c1efafe13a39
Zim <73538267+Mohamm4@users.noreply.github.com>
2020-11-27 18:46:55 +0600

    Zim.py

---
+++ Zim.py
@@ -0,0 +1,17 @@
+#!/bin/python2
+#This Tool is for accessing routersploit 
+import os
+import sys
+from termcolor import colored
+
+
+def menu():
+       os.system("clear")
+       print colored("""__Authors__ = threat9 & Mohammad Zim""","green")
+       raw_input("press enter")
+       menu()
+       
+def hack():
+       os.system("clear")
+       os.system("python rsf.py"
+       hack()
