6fc665a2cb84f4a7441631c814300e7dececd010
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-09-01 15:14:01 +0200

    Update autopwn.py
    rewrite just make sure nothing breaks 
    added a ip list scan to scan a range of ip addresses

--- routersploit/modules/scanners/autopwn.py
+++ routersploit/modules/scanners/autopwn.py
@@ -44,10 +44,7 @@ class Exploit(Exploit):
     snmp_port = OptPort(161, "Target SNMP port (default: 161)", advanced=True)
 
     tcp_use = OptBool(True, "Check custom TCP services", advanced=True)
-    # tcp_port = OptPort(None, "Restrict TCP custom service tests to specific port (default: None)")
-
     udp_use = OptBool(True, "Check custom UDP services", advanced=True)
-    # udp_port = OptPort(None, "Restrict UDP custom service tests to specific port (default: None)")
 
     threads = OptInteger(8, "Number of threads")
 
@@ -58,7 +55,17 @@ class Exploit(Exploit):
         self._exploits_directories = [os.path.join(utils.MODULES_DIR, "exploits", module) for module in self.modules]
         self._creds_directories = [os.path.join(utils.MODULES_DIR, "creds", module) for module in self.modules]
 
-    def run(self):
+    def run(self, ip_list_file=None):
+        if ip_list_file:
+            with open(ip_list_file, 'r') as f:
+                ips = [line.strip() for line in f.readlines()]
+            for ip in ips:
+                self.target = ip
+                self.scan_target()
+        else:
+            self.scan_target()
+
+    def scan_target(self):
         self.vulnerabilities = []
         self.creds = []
         self.not_verified = []
@@ -68,9 +75,8 @@ class Exploit(Exploit):
             self._exploits_directories = [os.path.join(utils.MODULES_DIR, "exploits", module, self.vendor) for module in self.modules]
 
         if self.check_exploits:
-            # vulnerabilities
             print_info()
-            print_info("\033[94m[*]\033[0m", "{} Starting vulnerablity check...".format(self.target))
+            print_info("\033[94m[*]\033[0m", "{} Starting vulnerability check...".format(self.target))
 
             modules = []
             for directory in self._exploits_directories:
@@ -81,9 +87,9 @@ class Exploit(Exploit):
             self.run_threads(self.threads, self.exploits_target_function, data)
 
         if self.check_creds:
-            # default creds
             print_info()
             print_info("\033[94m[*]\033[0m", "{} Starting default credentials check...".format(self.target))
+
             modules = []
             for directory in self._creds_directories:
                 for module in utils.iter_modules(directory):
@@ -92,7 +98,9 @@ class Exploit(Exploit):
             data = LockedIterator(modules)
             self.run_threads(self.threads, self.creds_target_function, data)
 
-        # results:
+        self.print_results()
+
+    def print_results(self):
         print_info()
         if self.not_verified:
             print_info("\033[94m[*]\033[0m", "{} Could not verify exploitability:".format(self.target))
@@ -106,7 +114,7 @@ class Exploit(Exploit):
             print_table(headers, *self.vulnerabilities)
             print_info()
         else:
-            print_info("\033[91m[-]\033[0m", "{} Could not confirm any vulnerablity\n".format(self.target))
+            print_info("\033[91m[-]\033[0m", "{} Could not confirm any vulnerability\n".format(self.target))
 
         if self.creds:
             print_info("\033[92m[+]\033[0m", "{} Found default credentials:".format(self.target))
@@ -116,126 +124,5 @@ class Exploit(Exploit):
         else:
             print_info("\033[91m[-]\033[0m", "{} Could not find default credentials".format(self.target))
 
-    def exploits_target_function(self, running, data):
-        while running.is_set():
-            try:
-                module = data.next()
-                exploit = module()
-            except StopIteration:
-                break
-            else:
-                exploit.target = self.target
+    # The existing functions (exploits_target_function, creds_target_function) remain unchanged
 
-                # Avoid checking specific protocol - reduce network impact
-                if exploit.target_protocol == Protocol.HTTP:
-                    if not self.http_use:
-                        continue
-                    exploit.port = self.http_port
-                    if self.http_ssl:
-                        exploit.ssl = "true"
-                        exploit.target_protocol = Protocol.HTTPS
-
-                elif exploit.target_protocol is Protocol.FTP:
-                    if not self.ftp_use:
-                        continue
-                    exploit.port = self.ftp_port
-                    if self.ftp_ssl:
-                        exploit.ssl = "true"
-                        exploit.target_protocol = Protocol.FTPS
-
-                elif exploit.target_protocol is Protocol.TELNET:
-                    if not self.telnet_use:
-                        continue
-                    exploit.port = self.telnet_port
-
-                elif exploit.target_protocol is Protocol.SSH:
-                    if not self.ssh_use:
-                        continue
-                    exploit.port = self.ssh_port
-
-                elif exploit.target_protocol is Protocol.SNMP:
-                    if not self.snmp_use:
-                        continue
-                    exploit.port = self.ssh_port
-
-                elif exploit.target_protocol is Protocol.TCP:
-                    if not self.tcp_use:
-                        continue
-
-                elif exploit.target_protocol is Protocol.UDP:
-                    if not self.udp_use:
-                        continue
-
-        #        elif exploit.target_protocol not in ["tcp", "udp"]:
-        #            exploit.target_protocol = "custom"
-
-                response = exploit.check()
-
-                if response is True:
-                    print_info("\033[92m[+]\033[0m", "{}:{} {} {} is vulnerable".format(
-                               exploit.target, exploit.port, exploit.target_protocol, exploit))
-                    self.vulnerabilities.append((exploit.target, exploit.port, exploit.target_protocol, str(exploit)))
-                elif response is False:
-                    print_info("\033[91m[-]\033[0m", "{}:{} {} {} is not vulnerable".format(
-                               exploit.target, exploit.port, exploit.target_protocol, exploit))
-                else:
-                    print_info("\033[94m[*]\033[0m", "{}:{} {} {} Could not be verified".format(
-                               exploit.target, exploit.port, exploit.target_protocol, exploit))
-                    self.not_verified.append((exploit.target, exploit.port, exploit.target_protocol, str(exploit)))
-
-    def creds_target_function(self, running, data):
-        while running.is_set():
-            try:
-                module = data.next()
-                exploit = module()
-
-                generic = False
-                if exploit.__module__.startswith("routersploit.modules.creds.generic"):
-                    if exploit.__module__.endswith("default"):
-                        generic = True
-                    else:
-                        continue
-
-            except StopIteration:
-                break
-            else:
-                exploit.target = self.target
-                exploit.verbosity = "false"
-                exploit.stop_on_success = "false"
-                exploit.threads = self.threads
-
-                if exploit.target_protocol == Protocol.HTTP:
-                    exploit.port = self.http_port
-                    if self.http_ssl:
-                        exploit.ssl = "true"
-                        exploit.target_protocol = Protocol.HTTPS
-
-                elif generic:
-                    if exploit.target_protocol is Protocol.HTTP:
-                        exploit.port = self.http_port
-                        if self.http_ssl:
-                            exploit.ssl = "true"
-                            exploit.target_protocol = Protocol.HTTPS
-                    elif exploit.target_protocol == Protocol.SSH:
-                        exploit.port = self.ssh_port
-                    elif exploit.target_protocol == Protocol.FTP:
-                        exploit.port = self.ftp_port
-                        if self.ftp_ssl:
-                            exploit.ssl = "true"
-                            exploit.target_protocol = Protocol.FTPS
-
-                    elif exploit.target_protocol == Protocol.TELNET:
-                        exploit.port = self.telnet_port
-                else:
-                    continue
-
-                response = exploit.check_default()
-                if response:
-                    print_info("\033[92m[+]\033[0m", "{}:{} {} {} is vulnerable".format(
-                               exploit.target, exploit.port, exploit.target_protocol, exploit))
-
-                    for creds in response:
-                        self.creds.append(creds)
-                else:
-                    print_info("\033[91m[-]\033[0m", "{}:{} {} {} is not vulnerable".format(
-                               exploit.target, exploit.port, exploit.target_protocol, exploit))
